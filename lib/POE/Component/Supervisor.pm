3e9b08c68acd003a7b2e7665d1358ec36ec38527
Yuval Kogman <nothingmuch@woobling.org>
2008-08-16 06:12:18 +0000

    version bump

--- lib/POE/Component/Supervisor.pm
+++ lib/POE/Component/Supervisor.pm
@@ -14,7 +14,7 @@ use Hash::Util::FieldHash::Compat qw(idhash);
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = "0.04";
+our $VERSION = "0.05";
 
 with qw(
     POE::Component::Supervisor::Interface
